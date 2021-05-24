808843f0ca8caf6e9b70a8078b365157921ec95f
DavidFQ2212 <kaoszcross12@gmail.com>
2021-05-24 18:54:15 -0500

    subida de archivos

---
+++ src/compra/Compra.java
@@ -0,0 +1,11 @@
+
+package compra;
+import java.util.*;
+public class Compra {
+
+    public static void main(String[] args) {
+       Vista vista= new Vista();
+       vista.setVisible(true);
+    }
+    
+}
